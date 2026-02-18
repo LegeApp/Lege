273313e547fb6bad88c7881199ee9797003141c2
Temp User <temp@example.com>
2026-02-18 12:09:40 -0800

    chore: untrack restored local dev assets from commit

--- drop_test.rs
+++
@@ -1,8 +0,0 @@
-struct A(&'static str);
-struct B(&'static str);
-impl Drop for A { fn drop(&mut self){ println!("drop {}", self.0); }}
-impl Drop for B { fn drop(&mut self){ println!("drop {}", self.0); }}
-
-struct Foo { a: A, b: B }
-
-fn main(){ let _f = Foo { a: A("A"), b: B("B") }; }
